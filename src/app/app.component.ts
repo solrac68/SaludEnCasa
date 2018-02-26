6d1f498fd8368ccba730a69530655982cc21ceba
Carlos Augusto Hincapié R <solrac.hincapie@gmail.com>
2018-02-26 11:34:52 -0500

    Ejemplos Iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'PlatziSquares';
+  a = 2;
+  b = 3;
+  listo = false;
+  //nombre = '' -->> Así es en JavaScript
+  nombre:string = '' // ASÍ ES EN typeScript
+  apellido:string = ''
+
+  constructor() {
+    setTimeout(() => {
+      this.listo = true;
+    },3000)
+  }
+
+  hacerAlgo(){
+    alert('Haciendo Algo')
+  }
 }
