e25c18bd9e961d99637b0c8c683b3ccb68007b99
Carlos Augusto Hincapié R <solrac.hincapie@gmail.com>
2018-03-06 16:24:16 -0500

    Hasta ejemplo Host Listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PlatziSquares';
   lugares:any = [
-    {active:true, nombre:'Florería la gardenia'},
-    {active:true, nombre:'Donas la pasadita'},
-    {active:true, nombre:'Veterinaria la huellitas felices'},
-    {active:false, nombre:'Sushi Suhiroll'},
-    {active:true, nombre:'Holtel la Gracia'},
-    {active:false, nombre:'Zapateria el clavo'}
+    {plan:'pagado',cercania:1,distancia:1,active:true, nombre:'Florería la gardenia'},
+    {plan:'gratuito',cercania:1,distancia:1.8,active:true, nombre:'Donas la pasadita'},
+    {plan:'gratuito',cercania:2,distancia:5,active:true, nombre:'Veterinaria la huellitas felices'},
+    {plan:'gratuito',cercania:3,distancia:10,active:false, nombre:'Sushi Suhiroll'},
+    {plan:'pagado',cercania:3,distancia:35,active:true, nombre:'Holtel la Gracia'},
+    {plan:'gratuito',cercania:3,distancia:120,active:false, nombre:'Zapateria el clavo'}
+  ];
+
+  personas:any = [
+    {edad:25, nombre:'Juan Antonio'},
+    {edad:18, nombre:'Elias Eduardo'},
+    {edad:20, nombre:'Sebastian'},
+    {edad:50, nombre:'Sushi Suhiroll'},
+    {edad:45, nombre:'Alvaro'},
+    {edad:23, nombre:'Gonzalo'}
   ];
   
   lat:number = 6.1577835;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms'
 
 import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,ResaltarDirective,ContarClicksDirective
   ],
   imports: [
     BrowserModule,
